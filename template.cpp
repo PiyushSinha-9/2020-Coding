83593e33ddb1744e40dd5dd719c6ca41cce64f32
PiyushSinha <piyush7924@gmail.com>
2020-05-05 17:49:51 +0530

    template added

---
+++ template.cpp
@@ -0,0 +1,64 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define F first
+#define S second
+#define print(x) for(auto iuiuiuiuiui:x){ cout<<iuiuiuiuiui<<" ";}br;
+#define print_array(array,size) for(ll i=0;i<size;i++){ cout<<array[i]<<" ";}br;
+#define br cout<<"\n"
+#define pb(a) push_back(a)
+#define mp(asd,fgh) make_pair(asd,fgh)
+#define all(c) c.begin(),c.end()
+#define check_ cout<<"yo";
+#define si(x)   scanf("%d",&x)
+#define sl(x)   scanf("%lld",&x)
+#define ss(s)   scanf("%s",s)
+#define pi(x)   printf("%d",x)
+#define pl(x)   printf("%lld",x)
+#define ps(s)   printf("%s",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define FILE_READ_IN freopen("input.txt","r",stdin);
+#define FILE_READ_OUT freopen("output.txt","w",stdout);
+#define FILE FILE_READ_IN FILE_READ_OUT  long long tt=clock();
+#define run_time cout << "\nTIME = " << clock() - tt << "\n"; tt = clock();
+
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+
+const int INF=1e9+9;
+// const ll INF=1e18L+5;
+const ll mod=1e9L+7;
+
+#pragma GCC target("popcnt")
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    FILE
+
+    ll test;
+    sl(test);
+    while(test--){
+        ll n,v;
+        sl(n);
+        sl(v);
+        cout<<n+v;br;
+        run_time;
+    }
+
+    
+}
